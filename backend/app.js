0d421c5c4e812551a07bc45f29c3e04e8dcf147a
Leonardo <leonardozanidev@gmail.com>
2025-11-14 15:30:50 -0300

    create some functions to handle memory and upload in the backend

--- backend/app.js
+++ backend/app.js
@@ -6,10 +6,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-require("./db/conn")
+require("./db/conn");
 
 const port = 3000;
 
-app.listen(port, async() => {
-    console.log(`Servidor iniciado na porta:${port}`)
+const memoryRoutes = require("./routes");
+
+app.use("/memories", memoryRoutes);
+
+app.listen(port, async () => {
+  console.log(`Servidor iniciado na porta:${port}`);
 });
---
+++ backend/routes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const router = express.Router();
+
+const upload = require("./helpers/upload");
+
+const { createMemory, getMemories } = require("./controllers/MemoryController");
+
+router.post(
+  "/",
+  upload.single("image"),
+  (req, res, next) => {
+    const image = req.file;
+    if (!image) {
+      return res.status(400).json({ msg: "Por favor, envie um arquivo!" });
+    }
+
+    next();
+  },
+  (req, res) => createMemory(req, res)
+);
+
+router.get("/", (req, res) => getMemories(req, res));
+
+module.exports = router;
