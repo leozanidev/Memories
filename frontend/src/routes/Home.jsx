d0a6ba29f102677c47eaa243953af8ccedfea28a
Leonardo <leonardozanidev@gmail.com>
2025-11-17 17:48:29 -0300

    Display memories in home page with useEffect, GET and useState

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import axios from "../axios-config.js";
+
+import { useState, useEffect } from "react";
+
+import { Link } from "react-router-dom";
 
 const Home = () => {
+  const [memories, setMemories] = useState([]);
+
+  useEffect(() => {
+    const getMemories = async () => {
+      const res = await axios.get("/memories");
+
+      setMemories(res.data);
+    };
+
+    getMemories();
+  }, []);
   return (
-    <div>
-      <h2>Home</h2>
+    <div className="home">
+      <h2>Confira as últimas Memórias</h2>
+      <div className="memories-container">
+        {memories.length > 0 && memories.map((memory) => <p>{memory.title}</p>)}
+      </div>
     </div>
   );
 };
