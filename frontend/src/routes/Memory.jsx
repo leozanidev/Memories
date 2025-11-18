c704135109cda8e4580cab9c326b8bd420329846
Leonardo <leonardozanidev@gmail.com>
2025-11-18 12:59:56 -0300

    Ended project with add comment function

--- frontend/src/routes/Memory.jsx
+++ frontend/src/routes/Memory.jsx
@@ -11,6 +11,9 @@ const Memory = () => {
   const [memory, setMemory] = useState(null);
   const [comments, setComments] = useState([]);
 
+  const [name, setName] = useState("");
+  const [text, setText] = useState("");
+
   useEffect(() => {
     const getMemory = async () => {
       const res = await axios.get(`/memories/${id}`);
@@ -22,6 +25,31 @@ const Memory = () => {
     getMemory();
   });
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const comment = { name, text };
+
+      const res = await axios.patch(
+        `/memories/${memory._id}/comment/`,
+        comment
+      );
+
+      const lastComment = res.data.memory.comments.pop();
+
+      setComments((comments) => [...comments, lastComment]);
+
+      setName("");
+      setText("");
+
+      toast.success(res.data.msg);
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.msg);
+    }
+  };
+
   if (!memory) return <p>Carregando...</p>;
 
   return (
@@ -31,12 +59,20 @@ const Memory = () => {
       <p>{memory.description}</p>
       <div className="comment-form">
         <h3>Envie seu comentário</h3>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>
-            <input type="text" placeholder="Insira seu nome" />
+            <input
+              type="text"
+              placeholder="Insira seu nome"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+            />
           </label>
           <label>
-            <textarea placeholder="Seu comentário"></textarea>
+            <textarea
+              placeholder="Seu comentário"
+              onChange={(e) => setText(e.target.value)}
+              value={text}></textarea>
           </label>
           <input type="submit" value="Enviar" className="btn" />
         </form>
