d88b53d90984cbc4fb61667f0808541125bfaa93
Leonardo <leonardozanidev@gmail.com>
2025-11-18 12:45:56 -0300

    Done memory page jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,6 +8,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // Pages
 import Home from "./routes/Home.jsx";
 import AddMemory from "./routes/AddMemory.jsx";
+import Memory from "./routes/Memory.jsx";
 
 const router = createBrowserRouter([
   {
@@ -21,6 +22,10 @@ const router = createBrowserRouter([
       {
         path: "/add-memory",
         element: <AddMemory />,
+      },
+      {
+        path: "/memories/:id",
+        element: <Memory />,
       },
     ],
   },
--- frontend/src/routes/Memory.jsx
+++ frontend/src/routes/Memory.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import axios from "../axios-config.js";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Memory = () => {
-  return (
-    <div>Memory</div>
-  )
-}
+  const { id } = useParams();
 
-export default Memory
+  const [memory, setMemory] = useState(null);
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    const getMemory = async () => {
+      const res = await axios.get(`/memories/${id}`);
+
+      setMemory(res.data);
+      setComments(res.data.comments);
+    };
+
+    getMemory();
+  });
+
+  if (!memory) return <p>Carregando...</p>;
+
+  return (
+    <div className="memory-page">
+      <img src={`${axios.defaults.baseURL}${memory.src}`} alt={memory.title} />
+      <h2>{memory.title}</h2>
+      <p>{memory.description}</p>
+      <div className="comment-form">
+        <h3>Envie seu comentário</h3>
+        <form>
+          <label>
+            <input type="text" placeholder="Insira seu nome" />
+          </label>
+          <label>
+            <textarea placeholder="Seu comentário"></textarea>
+          </label>
+          <input type="submit" value="Enviar" className="btn" />
+        </form>
+      </div>
+      <div className="comments-container">
+        <h3>Comentários({comments.length})</h3>
+        {comments.length === 0 && <p>Não há comentários</p>}
+        {comments.length > 0 &&
+          comments.map((comment) => (
+            <div className="comment" key={comment._id}>
+              <p className="comment-name">{comment.name}</p>
+              <p className="comment-text">{comment.text}</p>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Memory;
